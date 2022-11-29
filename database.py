761dbd5880362bba2692b4dfc691c372c313cd35
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2022-11-28 21:36:08 -0500

    initial file structure

---
+++ database.py
@@ -0,0 +1,6 @@
+class Database:
+    def __init__(self):
+        self.movie_list= []
+
+    def add_movie(self):
+        pass
---
+++ movie.py
@@ -0,0 +1,6 @@
+class Movie:
+    def __init__(self, genre, title, director):
+        self.genre = genre
+        self.title = title
+        self.director = director
+        self.cast_members = []
