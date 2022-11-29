c7de4e128142a7d5ccc89f2291a0c936dea5096a
Chris Wood <wchristopherwood912@gmail.com>
2022-11-28 19:03:50 -0800

    working on movie

--- movie.py
+++ movie.py
@@ -4,3 +4,9 @@ class Movie:
         self.title = title
         self.director = director
         self.cast_members = []
+
+    def __repr__(self):
+        output = f"Title:{self.title}\nDirector:{self.director}\nGenre:{self.genre}\n"
+
+        # for cast in self.cast_members:
+        return output
