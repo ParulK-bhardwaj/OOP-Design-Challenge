eaf232c58a2fb2c3abd562ad01c49c76343ae3cb
Chris Wood <wchristopherwood912@gmail.com>
2022-11-29 19:54:31 -0800

    added add_cast method

--- movie.py
+++ movie.py
@@ -5,8 +5,20 @@ class Movie:
         self.director = director
         self.cast_members = []
 
-    def __repr__(self):
+    def __str__(self):
         output = f"Title:{self.title}\nDirector:{self.director}\nGenre:{self.genre}\n"
 
         # for cast in self.cast_members:
         return output
+
+    def add_cast(self, name, role):
+        """
+        adds a dict object representing a cast member to cast_members list
+        """
+
+        member = {
+            "name":name,
+            "role":role
+        }
+
+        self.cast_members.append(member)
