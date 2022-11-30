c4abf0aff2bedc7dba957252beb5743ba3ca3c5c
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2022-11-30 00:10:36 -0500

    added str method in movie

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
-class Database:
+from movie import Movie;
+
+class Database(Movie):
     def __init__(self):
         self.movie_list= []
 
-    def add_movie(self):
-        pass
+    def add_movie(self,  ):
+        self.movie_list.append(Pride)
--- movie.py
+++ movie.py
@@ -1,6 +1,22 @@
 class Movie:
-    def __init__(self, genre, title, director):
-        self.genre = genre
-        self.title = title
-        self.director = director
-        self.cast_members = []
+    def __init__(self, title, year, genre, director, cast_members, language):
+        self.title = title #string
+        self.year = year #number
+        self.genre = genre #string
+        self.director = director #string
+        self.cast_members = cast_members #list
+        self.language = language #string
+
+    def __str__(self):
+        return f"""
+        Title: {self.title}
+        Year: {self.year}
+        Genre: {self.genre}
+        Director: {self.director}
+        Cast: {', '.join(self.cast_members)}
+        Language: {self.language}
+        """
+
+if __name__ == "__main__":
+    pride = Movie('Pride and Prejudice', 2005, 'romantic-drama', 'Joe Wright', ['Keira Knightley', 'Matthew Macfadyen'], 'English',)
+    print(pride.__str__())
