3cbae17614594862437318ea87ce7fc6ffd3cbce
Chris Wood <wchristopherwood912@gmail.com>
2022-12-01 19:30:07 -0800

    fixed import error and uncommented run stuff

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from StreamingService import StreamingService
+from streamingService import StreamingService
 
 class App: 
   def __init__(self):
@@ -11,15 +11,15 @@ class App:
     self.streaming_services.pop(streaming_service.id)
 
 if __name__ == '__main__':
-  # app = App()
-  # s1 = StreamingService("s1", "movie", 80)
-  # s2 = StreamingService("s2", "movie", 80)
-  # s3 = StreamingService("s3", "movie", 80)
+  app = App()
+  s1 = StreamingService("s1", "movie", 80)
+  s2 = StreamingService("s2", "movie", 80)
+  s3 = StreamingService("s3", "movie", 80)
 
-  # app.add_streaming_service(s1)
-  # app.add_streaming_service(s2)
-  # app.add_streaming_service(s3)
-  # print(app.streaming_services)
+  app.add_streaming_service(s1)
+  app.add_streaming_service(s2)
+  app.add_streaming_service(s3)
+  print(app.streaming_services)
 
-  # app.remove_streaming_service(s2)
-  # print(app.streaming_services)
+  app.remove_streaming_service(s2)
+  print(app.streaming_services)
