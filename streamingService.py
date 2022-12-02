d6c4e058e9f3001fc715cdcd2ef946699a015556
Chris Wood <wchristopherwood912@gmail.com>
2022-12-02 11:19:20 -0800

    WIP: 2fecd92 added comments explaing public/private choices

--- streamingService.py
+++ streamingService.py
@@ -1,16 +1,17 @@
 import itertools
+from database import Database
 
 class StreamingService: 
   new_id = itertools.count()
-  def __init__(self, name, description, subscription_fee):
+  def __init__(self, name, description, subscription_fee, movie_database):
     self.id = next(StreamingService.new_id)
     self.name = name 
     self.description = description 
     self.subscription_fee = subscription_fee 
-    self.movies = {}
+    self.movie_database = movie_database
 
-  def add_movie(self, movie): 
-    self.movies[movie.id] = movie
+    print(self.id)
 
-  def remove_movie(self, movie):
-    self.movies.pop(movie.id)
+  #TODO __str__() print name fee, and description
+
+  #TODO pay_subscription(is_annual) charge only do not check timing
