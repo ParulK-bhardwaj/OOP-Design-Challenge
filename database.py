02550d4d335385a4ef8454ee732fa90494b9c588
Chris Wood <wchristopherwood912@gmail.com>
2022-12-02 11:27:34 -0800

    [add] filter_by_genre()

--- database.py
+++ database.py
@@ -2,14 +2,14 @@ from movie import Movie;
 
 class Database(Movie):
     def __init__(self):
-        self.movie_list= []
+        self.__movie_list= []
     
     #public because it needs to be accessed outside of this class and subclasses
     def add_movie(self, movie):
-        self.movie_list.append(movie)
+        self.__movie_list.append(movie)
     #public because it needs to be accessed outside of this class and subclasses
     def remove_movie(self, movie):
-        self.movie_list.remove(movie)
+        self.__movie_list.remove(movie)
     #cannot be used outside of class therefore is private
     def __str__(self):
         out = ""
@@ -20,4 +20,10 @@ class Database(Movie):
         return out
     #public because it needs to be accessed outside of this class and subclasses
     def filter_by_genre(self, genre):
-        pass
+        filtered_movies = []
+
+        for movie in self.__movie_list:
+            if genre == movie.genre:
+                filtered_movies.append(movie)
+
+        return filtered_movies
